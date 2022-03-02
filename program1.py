8e82ba9e52ff28fa6ca70ac1e2ca4a1a266e4e54
pgcolin <pgrabczak@gmail.com>
2022-03-02 21:42:38 +0100

    zmiana3

--- program1.py
+++ program1.py
@@ -5,8 +5,8 @@ x = input('wpisz coś: ')
 print(x)
 print('co ty wpisałeś?')
 
-y = input('wpisz 1 cyfre dod dodania')
-z = input('wpisz 2 cyfre dod dodania')
+y = input(int('wpisz 1 cyfre dod dodania: '))
+z = input(int('wpisz 2 cyfre dod dodania: '))
 w = y + z
 print('wynik dodania 1 i 2 cyfry wynosi: ', w)
 
